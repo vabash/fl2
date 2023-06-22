3d2bf656f60e4612d0edf1cc4f352c51cafd3150
vabash <98648766+vabash@users.noreply.github.com>
2023-06-22 19:55:35 +0300

    Первоначальные файлы для проекта MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Открыть эту страницу в [https://vabash.github.io/fl2/](https://vabash.github.io/fl2/)
+
+## Использовать это расширение
+
+Этот репозиторий может быть добавлен в MakeCode как **расширение**.
+
+* открыть [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* нажмите на **Новый проект**
+* нажмите **Расширения** в меню-шестерёнке
+* найдите **https://github.com/vabash/fl2** и импортируйте
+
+## Править этот проект
+
+Редактировать этот репозиторий в MakeCode.
+
+* открыть [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* нажмите на **Импорт**, затем **Импорт URL**
+* вставьте **https://github.com/vabash/fl2** и нажмите Импорт
+
+#### Метаданные (используются для поиска, рендеринга)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,160 @@
+basic.showLeds(`
+    # # . # #
+    # # . # #
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . # #
+    . . . # #
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # . # #
+    # # . # #
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # . . .
+    # # . . .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # . # #
+    # # . # #
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # . # #
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        # # . # #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . . . .
+        # # . # #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        # # . # #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # . # .
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        # # # # #
+        # # # # #
+        `)
+})
